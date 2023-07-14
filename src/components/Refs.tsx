580855e726e9bc64db30fa9eea7f36167c1b2f1e
Gabriel <gabrigomez15@gmail.com>
2023-07-14 17:27:02 -0300

    fix: wip in reference format

--- src/components/Refs.tsx
+++ src/components/Refs.tsx
@@ -5,7 +5,21 @@ export const Refs: FC<Work> = ({...props}) => {
   const author = (props.author).split(' ')
   const firstName = (author[author.length -1]).toUpperCase()
   const lastName = author[0]
-  
+
+  const workData = {
+    lastName: firstName + ',',
+    firstName: lastName + '.',
+    title: props.title + ',',
+    publisher: props.publisher + ',',
+    locale: props.locale  + ',',
+    year: props.year + '.'
+  }
+
+  // const array = Object.values({...workData}).splice(1, 1)
+  //const object = Object.values({...workData}).join(' ')
+
+  const object = Object.values({...workData})
+
   return (
     <div className={`w-full flex flex-col ${props.className ? props.className : 'text-black'}`}>
       <div>
@@ -13,54 +27,13 @@ export const Refs: FC<Work> = ({...props}) => {
           REFERÊNCIA
         </p>
       </div>
-      <div className="flex flex-col justify-center items-center sm:flex-row">
+      <div className="flex m-2 justify-center items-center sm:flex-row">
         <div className="flex">
-          <p className="mr-1">
-            {firstName}, 
-          </p>
-          <p className="mr-1">
-            {lastName}.
-          </p>       
-          <p className="font-bold mr-1">
-            {props.title}.
-          </p>     
-        </div>
-        <div className="flex">
-          <p className="mr-1">
-            {props.publisher}:
-          </p>
-          <p className="mr-1">
-            {props.locale},
-          </p>
-          {props.volume && (
-            <p className="mr-1">
-              v. {props.volume},
-            </p>
-          )}
-          {props.volume && (
-            <p className="mr-1">
-              n. {props.fascicle},
-            </p>
-          )}
-          {props.initialPage && (
-            <p className="mr-1">
-              p. {props.initialPage}-
-            </p>
-          )}
-          {props.finalPage && (
-            <p className="mr-1">
-              {props.finalPage},
-            </p>
-          )}
-          {props.mounth && (
-            <p className="mr-1">
-              {props.mounth}.
-            </p>
-          )}
-          <p className="mr-1">
-            {props.year}.
-          </p>
-        </div>
+          <p>          
+            {firstName}, {lastName}. <span className="font-bold">{props.title}</span>. {props.publisher}: 
+            {props.locale}, {props.year}.
+          </p>          
+        </div>       
       </div>
     </div>
   )
